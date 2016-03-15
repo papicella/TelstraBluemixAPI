7ed6b68c19c33bcfce5e74719c46d83671a36529
pasapicella <pas.apicella@tpg.com.au>
2016-03-15 21:37:57 +1100

    Added Swagger API Support

--- src/main/java/ibm/com/bluemix/telstra/TelstraBluemixApiApplication.java
+++ src/main/java/ibm/com/bluemix/telstra/TelstraBluemixApiApplication.java
@@ -2,11 +2,41 @@ package ibm.com.bluemix.telstra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
+@EnableSwagger2
 @SpringBootApplication
-public class TelstraBluemixApiApplication {
+public class TelstraBluemixApiApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(TelstraBluemixApiApplication.class, args);
+	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("telstraapis")
+				.apiInfo(apiInfo())
+				.select()
+				.paths(regex("/apis.*"))
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Telstra Bluemix API Demo with Swagger")
+				.description("Telstra Bluemix API Demo with Swagger")
+				.version("1.0")
+				.contact("Pas Apicella")
+				.license("Apache License Version 2.0")
+				.build();
 	}
 }
--- src/main/java/ibm/com/bluemix/telstra/rest/BluemixRest.java
+++ src/main/java/ibm/com/bluemix/telstra/rest/BluemixRest.java
@@ -4,6 +4,9 @@ import ibm.com.bluemix.telstra.commands.Command;
 import ibm.com.bluemix.telstra.commands.OsCommand;
 import ibm.com.bluemix.telstra.main.Constants;
 import ibm.com.bluemix.telstra.main.Result;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/apis")
 public class BluemixRest
 {
+    @ApiOperation(value = "createOrg", nickname = "createOrg")
     @RequestMapping(method=RequestMethod.GET, value="/create/{orgName}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Result.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
     public Result createOrg(@PathVariable String orgName)
     {
         Command osCommand = new OsCommand();
@@ -35,7 +45,14 @@ public class BluemixRest
         return result;
     }
 
+    @ApiOperation(value = "deleteOrg", nickname = "deleteOrg")
     @RequestMapping(method=RequestMethod.GET, value="/delete/{orgName}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Result.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
     public Result deleteOrg(@PathVariable String orgName)
     {
         Command osCommand = new OsCommand();
